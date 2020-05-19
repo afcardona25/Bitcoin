09628bd9490a92b7bad8967fb391d470c46aeaef
Felipe Cardona <af.cardona9509@gmail.com>
2020-05-19 16:28:47 -0500

    redireccion

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function validateForm() {
+    var mail = document.forms["validacion"]["email"].value;
+    var password = document.forms["validacion"]["password"].value;
+    alert("Bienvenido/a")
+    window.open("perfil.html")
+  }
